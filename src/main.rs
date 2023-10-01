72fa8398ee4f55630023ae2a42b22400e26ba818
Abhay Raj Singh <abhayonlyone@gmail.com>
2023-10-02 01:54:30 +0530

    ci fix

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,64 @@
 #![no_std]
 #![no_main]
 
+
 mod application;
 mod commons;
 mod dexter_core;
 mod ui;
 
-use dexter_core::hasher::no_hasher::NoHasher;
 
 extern crate alloc;
-
-use esp32_hal::{
-    clock::ClockControl,
-    gpio::IO,
-    pac::Peripherals,
-    prelude::*,
-    timer::TimerGroup,
-    Delay,
-    // Delay,
-    // Serial,
-    Rtc,
-};
-
+use core::mem::MaybeUninit;
 use esp_backtrace as _;
+use esp_println::println;
+use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
 
-use crate::dexter_core::{store::no_store::NoStore, Core, DefaultCore};
+use esp_wifi::{initialize, EspWifiInitFor};
+
+use hal::{timer::TimerGroup, Rng};
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
 fn init_heap() {
-    const HEAP_SIZE: usize = 128 * 1024;
-
-    extern "C" {
-        static mut _heap_start: u32;
-        static mut _heap_end: u32;
-    }
+    const HEAP_SIZE: usize = 32 * 1024;
+    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
 
     unsafe {
-        let heap_start = &_heap_start as *const _ as usize;
-        let heap_end = &_heap_end as *const _ as usize;
-        assert!(
-            heap_end - heap_start > HEAP_SIZE,
-            "Not enough available heap memory."
-        );
-        ALLOCATOR.init(heap_start as *mut u8, HEAP_SIZE);
+        ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
     }
 }
-
-#[xtensa_lx_rt::entry]
+#[entry]
 fn main() -> ! {
     init_heap();
-    let peripherals = Peripherals::take().unwrap();
-    let system = peripherals.DPORT.split();
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let peripherals = Peripherals::take();
+    let mut system = peripherals.DPORT.split();
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let mut delay = Delay::new(&clocks);
 
-    // let serial0 = Serial::new(peripherals.UART0);
-
-    // Disable the RTC and TIMG watchdog timers
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
-    let mut wdt1 = timer_group1.wdt;
-
-    let delay = Delay::new(&clocks);
-
-    rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
-
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-
-    execute(io, delay)
-}
-
-fn execute(io: IO, delay: Delay) -> ! {
-    const DIGITS: usize = 5;
-    const KEYS: usize = 4;
-
-    // ------- Core -------
-    let hasher = NoHasher::new();
-    let store = NoStore::new();
-
-    let core = DefaultCore::new(hasher, store, [3; DIGITS]);
-
-    // looper::<KEYS>(keypad);
-
-    loop {}
+    // setup logger
+    // To change the log_level change the env section in .cargo/config.toml
+    // or remove it and set ESP_LOGLEVEL manually before running cargo run
+    // this requires a clean rebuild because of https://github.com/rust-lang/cargo/issues/10358
+    esp_println::logger::init_logger_from_env();
+    log::info!("Logger is setup");
+    println!("Hello world!");
+    let timer = TimerGroup::new(
+        peripherals.TIMG1,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    )
+    .timer0;
+    let _init = initialize(
+        EspWifiInitFor::Wifi,
+        timer,
+        Rng::new(peripherals.RNG),
+        system.radio_clock_control,
+        &clocks,
+    )
+    .unwrap();
+    loop {
+        println!("Loop...");
+        delay.delay_ms(500u32);
+    }
 }
